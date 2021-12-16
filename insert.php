54607c7411d3680e1cfb80ed3a2189e7b347bb68
theplenoh <tylrd529@gmail.com>
2021-12-16 16:13:17 +0900

    Advancement

---
+++ insert.php
@@ -0,0 +1,15 @@
+<?php
+include "common.php";
+
+if(!$_POST['content'])
+    die("받아 온 포스팅 내용이 없습니다.");
+
+$content = sanitize($_POST['content']);
+
+$query = "INSERT INTO monolog_entries (content, wdate) VALUES('{$content}', NOW())";
+
+mysqli_query($conn, $query);
+mysqli_close($conn);
+
+header("Location: index.php");
+?>
