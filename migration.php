457f271ff8dd5d18e46e0f563f07f3a8271f37b2
Plenoh <tylrd629@gmail.com>
2022-04-10 15:01:54 +0900

    Updates

--- migration.php
+++ migration.php
@@ -19,7 +19,7 @@ exit;
 <html lang="ko">
 <head>
     <meta charset="utf-8">
-    <title><?php echo $screenname; ?>'s Monologue</title>
+    <title>Migration - <?php echo $screenname; ?>'s Monologue</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link crossorigin="anonymous" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" rel="stylesheet">
 </head>
