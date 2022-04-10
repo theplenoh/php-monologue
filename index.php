06154472893a0c27635ed919bcfa85ff840414d8
theplenoh <tylrd529@gmail.com>
2022-04-10 15:29:42 +0900

    Show links in white for private posts

--- index.php
+++ index.php
@@ -32,6 +32,7 @@ $page_max = ceil($total / $page_size);
     <title><?php echo $screenname; ?>'s Monologue</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link crossorigin="anonymous" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
