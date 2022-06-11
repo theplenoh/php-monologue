b94bb71eadd879bb4131cc3f99b0e555f085c5b1
theplenoh <tylrd529@gmail.com>
2022-06-11 10:40:56 +0900

    Take care of the case which a user isn't logged in

--- make_private.php
+++ make_private.php
@@ -1,5 +1,17 @@
 <?php
 require_once "common.php";
 
+session_start();
 
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true)
+{
+$message = "로그인을 해주세요.";
+echo<<<EOT
+<script>
+alert("{$message}");
+location.href="login.php";
+</script>
+EOT;
+exit;
+}
 ?>
